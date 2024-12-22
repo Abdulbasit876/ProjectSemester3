fcb2782c68feb61ebabda692ca8aa9194a50778c
“Basit” <malikbaistawanzada@gmail.com>
2024-12-21 21:31:18 -0800

    Project Base Setup

---
+++ BackEnd/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+port=8080
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+@app.route('/register')
+def register():
+    return 'Registering user...'
+if __name__ == '__main__':
+    app.run(debug=True,port=port)
